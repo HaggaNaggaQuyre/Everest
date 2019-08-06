1dae9591900c78eed33ea4801f7e9b5a18d8f41e
HaggaNaggaQuyre <jahndiez@icloud.com>
2019-08-06 02:56:32 +0200

    .

--- app/src/main/java/com/example/everest/MainActivity.java
+++ app/src/main/java/com/example/everest/MainActivity.java
@@ -7,7 +7,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.DispatchKeyEvent()onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
