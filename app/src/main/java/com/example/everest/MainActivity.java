cd5a6cbb4feededd85e73c00e928571d2224f969
HaggaNaggaQuyre <jahndiez@icloud.com>
2019-08-06 16:23:53 +0200

    .

--- app/src/main/java/com/example/everest/MainActivity.java
+++ app/src/main/java/com/example/everest/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.everest;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    EditText PenisText;
+    Button SchwanzButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String Löres = "Penis";
+        PenisText = (EditText) findViewById(R.layout.editText);
+        SchwanzButton = (Button) findViewById(R.layout.button);
+
+        SchwanzButton.setOnClickListener(this);
+
+
+
+
     }
 }
